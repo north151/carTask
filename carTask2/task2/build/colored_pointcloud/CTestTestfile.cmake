a5ca974a4f2115f11b8a61e37e9c6c82bd8dc7d3
north151 <3342503155@qq.com>
2023-11-13 13:06:15 +0800

    update

--- carTask2/task2/build/colored_pointcloud/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agilex/task2/src/colored_pointcloud
-# Build directory: /home/agilex/task2/build/colored_pointcloud
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
